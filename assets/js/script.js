c20a6598a5f6fe102231a11a26c1b52301c5979c
Aracelis <aracelis.salas.v@gmail.com>
2022-04-08 18:44:03 -0400

    navrrousel

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){ // necesario para empezar a usar script
+
+
+/*carrousel */   
+    var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel)
+
+});
