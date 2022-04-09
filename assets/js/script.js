d29b9e2ac464f17d19d69644f2d5287d9ae8c53c
Aracelis <aracelis.salas.v@gmail.com>
2022-04-08 22:45:16 -0400

    footer y tooltips

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,11 @@ $(document).ready(function(){ // necesario para empezar a usar script
     var myCarousel = document.querySelector('#myCarousel')
 var carousel = new bootstrap.Carousel(myCarousel)
 
+
+$('[data-toggle="tooltip"]').tooltip();
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 });
