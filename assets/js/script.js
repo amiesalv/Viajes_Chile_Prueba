bb9d8f3d0160551af26ee39f2aff31bbb8bf555e
Aracelis <aracelis.salas.v@gmail.com>
2022-04-09 13:18:42 -0400

    IDENTACION Y ORDEN

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){ // necesario para empezar a usar script
+$(document).ready(function () { // necesario para empezar a usar script
 
 
-/*carrousel */   
-    var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel)
+  /*carrousel */
+  var myCarousel = document.querySelector('#myCarousel')
+  var carousel = new bootstrap.Carousel(myCarousel)
 
-/*tooltips*/
-$('[data-toggle="tooltip"]').tooltip();
+  /*tooltips*/
+  $('[data-toggle="tooltip"]').tooltip();
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
 });
